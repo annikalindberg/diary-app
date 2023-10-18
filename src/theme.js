f4b6af91a35fef16735f4e38462878d589f465eb
Annika Lindberg <annika.edit.lindberg@gmail.com>
2023-10-18 16:01:02 +0200

    ..

--- src/theme.js
+++ src/theme.js
@@ -8,15 +8,15 @@ const theme = {
         lightbackground: '#E2D5D9',
         text: '#212121',
         lighttext: '#e7cee3',
-        primarydark: '#212121',
+        primarydark: '#202411',
         cta: '#e9dcacf0',
         ctahover: '#8e825770',
         hover: '#E2D5D9',
     },
-    fonts: {
+ /*    fonts: {
         primaryFont: "'Roboto', sans-serif",
         secondaryFont: "'Open Sans', sans-serif",
-    },
+    }, */
     spacing: {
         smallSpace: '8px',
         mediumSpace: '16px',
